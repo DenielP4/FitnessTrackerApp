6574c2e14e0803df43fb07122e0a5a018185c79e
Daniil Popov <popov@example.com>
2024-02-15 19:02:18 +0300

    DayFinishFragment

---
+++ app/src/main/java/com/danielp4/fitnesstracker/fragments/DayFinishFragment.kt
@@ -0,0 +1,40 @@
+package com.danielp4.fitnesstracker.fragments
+
+import android.os.Bundle
+import android.os.CountDownTimer
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.appcompat.app.AppCompatActivity
+import androidx.fragment.app.Fragment
+import com.danielp4.fitnesstracker.databinding.DayFinishBinding
+import com.danielp4.fitnesstracker.databinding.WaitingFragmentBinding
+import com.danielp4.fitnesstracker.utils.FragmentManager
+import com.danielp4.fitnesstracker.utils.TimeUtils
+import pl.droidsonroids.gif.GifDrawable
+
+
+class DayFinishFragment : Fragment() {
+
+    private lateinit var binding: DayFinishBinding
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        binding = DayFinishBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        binding.imgMain.setImageDrawable(GifDrawable((activity as AppCompatActivity).assets, "love-chill.gif"))
+        binding.btnDone.setOnClickListener {
+            FragmentManager.setFragment(DaysFragment.newInstance(), activity as AppCompatActivity)
+        }
+    }
+
+    companion object {
+        @JvmStatic
+        fun newInstance() = DayFinishFragment()
+    }
+}
