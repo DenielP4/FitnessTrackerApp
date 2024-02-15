750c05700528c83c3e431c2aaa048658565eb55b
Daniil Popov <popov@example.com>
2024-02-15 18:59:26 +0300

    MainActivity refactor

--- app/src/main/java/com/danielp4/fitnesstracker/MainActivity.kt
+++ app/src/main/java/com/danielp4/fitnesstracker/MainActivity.kt
@@ -2,10 +2,26 @@ package com.danielp4.fitnesstracker
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.danielp4.fitnesstracker.databinding.ActivityMainBinding
+import com.danielp4.fitnesstracker.fragments.DaysFragment
+import com.danielp4.fitnesstracker.utils.FragmentManager
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        FragmentManager.setFragment(DaysFragment.newInstance(), this)
+        setSupportActionBar(binding.toolbar)
+    }
+
+    override fun onBackPressed() {
+        if (FragmentManager.currentFragment is DaysFragment) {
+            super.onBackPressed()
+        } else {
+            FragmentManager.setFragment(DaysFragment.newInstance(), this)
+        }
     }
 }
